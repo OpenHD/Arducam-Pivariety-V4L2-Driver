49bee68ac28f2632d04c76f339c4e4eb8eda9ed2
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-18 23:55:16 +0200

    other syntax

--- Release/install_driver.sh
+++ Release/install_driver.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+install_drivers() {
+echo "Installing Arducam-Pivariety-V4L2-Driver..."
+echo "--------------------------------------"
+sudo install -p -m 755 ./arducam_camera_selector.sh /usr/bin/
+sudo install -p -m 644 ./bin/$uname/arducam.ko  /lib/modules/$uname/kernel/drivers/media/i2c/
+sudo install -p -m 644 ./bin/$uname/arducam.dtbo /boot/overlays/
+sudo install -p -m 644 ./bin/$uname/imx519.ko /lib/modules/$uname/kernel/drivers/media/i2c/
+sudo install -p -m 644 ./bin/$uname/ak7375.ko /lib/modules/$uname/kernel/drivers/media/i2c/ 
+sudo install -p -m 644 ./bin/$uname/imx519.dtbo /boot/overlays/
+sudo /sbin/depmod -a $uname
+}
+
 uname="5.15.61-v7+"
 install_drivers
 
@@ -15,17 +28,7 @@ install_drivers
     #uname=$(uname -r)
     #install_drivers();
 
-install_drivers() {
-echo "Installing Arducam-Pivariety-V4L2-Driver..."
-echo "--------------------------------------"
-sudo install -p -m 755 ./arducam_camera_selector.sh /usr/bin/
-sudo install -p -m 644 ./bin/$uname/arducam.ko  /lib/modules/$uname/kernel/drivers/media/i2c/
-sudo install -p -m 644 ./bin/$uname/arducam.dtbo /boot/overlays/
-sudo install -p -m 644 ./bin/$uname/imx519.ko /lib/modules/$uname/kernel/drivers/media/i2c/
-sudo install -p -m 644 ./bin/$uname/ak7375.ko /lib/modules/$uname/kernel/drivers/media/i2c/ 
-sudo install -p -m 644 ./bin/$uname/imx519.dtbo /boot/overlays/
-sudo /sbin/depmod -a $uname
-}
+
 awk 'BEGIN{ count=0 }       \
 {                           \
     if($1 == "dtoverlay=arducam-pivariety"){       \
