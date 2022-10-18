2033e9cc0d6eefe324f55107dd80108b2ce8f8ea
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-18 20:51:33 +0200

    update libcamera

--- Release/install_driver.sh
+++ Release/install_driver.sh
@@ -30,6 +30,12 @@ awk 'BEGIN{ count=0 }       \
         system("sudo sh -c '\''echo dtoverlay=imx519 >> /boot/config.txt'\''"); \
     }                       \
 }' /boot/config.txt
+
+wget -O install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
+chmod +x install_pivariety_pkgs.sh
+./install_pivariety_pkgs.sh -p libcamera_dev
+./install_pivariety_pkgs.sh -p libcamera_apps
+
 esac
 
         
