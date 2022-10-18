f3f9d1fcc02f83e63d1abe6bc781c39eaebcbc8c
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-18 20:50:24 +0200

    update drivers and install imx519

--- Release/install_driver.sh
+++ Release/install_driver.sh
@@ -4,27 +4,32 @@ echo "--------------------------------------"
 sudo install -p -m 755 ./arducam_camera_selector.sh /usr/bin/
 sudo install -p -m 644 ./bin/$(uname -r)/arducam.ko  /lib/modules/$(uname -r)/kernel/drivers/media/i2c/
 sudo install -p -m 644 ./bin/$(uname -r)/arducam.dtbo /boot/overlays/
+uname=$(uname -r)
+sudo install -p -m 644 ./bin/$uname/imx519.ko /lib/modules/$uname/kernel/drivers/media/i2c/
+sudo install -p -m 644 ./bin/$uname/ak7375.ko /lib/modules/$uname/kernel/drivers/media/i2c/ 
+sudo install -p -m 644 ./bin/$uname/imx519.dtbo /boot/overlays/
 sudo /sbin/depmod -a $(uname -r)
 awk 'BEGIN{ count=0 }       \
 {                           \
-    if($1 == "dtoverlay=arducam"){       \
+    if($1 == "dtoverlay=arducam-pivariety"){       \
         count++;            \
     }                       \
 }END{                       \
     if(count <= 0){         \
-        system("sudo sh -c '\''echo dtoverlay=arducam >> /boot/config.txt'\''"); \
+        system("sudo sh -c '\''echo dtoverlay=arducam-pivariety >> /boot/config.txt'\''"); \
     }                       \
 }' /boot/config.txt
-echo "reboot now?(y/n):"
-read USER_INPUT
-case $USER_INPUT in
-'y'|'Y')
-    echo "reboot"
-    sudo reboot
-;;
-*)
-    echo "cancel"
-;;
+
+awk 'BEGIN{ count=0 }       \
+{                           \
+    if($1 == "dtoverlay=imx519"){       \
+        count++;            \
+    }                       \
+}END{                       \
+    if(count <= 0){         \
+        system("sudo sh -c '\''echo dtoverlay=imx519 >> /boot/config.txt'\''"); \
+    }                       \
+}' /boot/config.txt
 esac
 
         
