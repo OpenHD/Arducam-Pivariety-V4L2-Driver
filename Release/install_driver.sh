57a49a4388600411b34bc4d4d19a8517c37b8cc2
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-29 23:37:30 +0200

    update libcamera-bins

--- Release/install_driver.sh
+++ Release/install_driver.sh
@@ -44,8 +44,8 @@ awk 'BEGIN{ count=0 }       \
 echo -e "remove libcamera0"
 echo ""
 sudo apt remove -y libcamera0
-sudo apt install -y ../libcamera-bins/libcamera-dev-0.0.9-bullseye-armhf.deb
-sudo apt install -y ../libcamera-bins/libcamera-apps-0.0.9-bullseye-armhf.deb
+sudo apt install -y ../libcamera-bins/libcamera-dev-0.0.10-bullseye-armhf.deb
+sudo apt install -y ../libcamera-bins/libcamera-apps-0.0.10-bullseye-armhf.deb
 
 
         
